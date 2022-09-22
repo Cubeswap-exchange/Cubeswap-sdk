d2cfcbddc41138ab9b786aa89446ddd5e49ad946
kisleav <kisleav@users.noreply.github.com>
2022-09-22 11:14:54 +0300

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | number | string
+
+export enum ChainId {
+  MAINNET = 1818,
+  TESTNET = 1819
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+
+export const FACTORY_ADDRESS_MAP = {
+  [ChainId.MAINNET]: FACTORY_ADDRESS,
+  [ChainId.TESTNET]: '0x8569310f94703cd66caff93099a5e1bbdaae7d33'
+}
+
+export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+
+export const INIT_CODE_HASH_MAP = {
+  [ChainId.MAINNET]: INIT_CODE_HASH,
+  [ChainId.TESTNET]: '0xa0fe4de06e1b8210c5bf8b3a27ace7ff9081ce95444e84554b8109878ab53ee9'
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(9975)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
